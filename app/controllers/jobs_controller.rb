d96126366d335fbd46160d3127bb593624a09860
Patrick Leonardo <patrickwreis@gmail.com>
2016-07-11 20:47:21 -0300

    edit jobs

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -10,16 +10,34 @@ class JobsController < ApplicationController
   end
 
   def create
-    @job = Job.new(params.require(:job).permit(:title, :company_id, :description, :location, :category, :featured))
+    @job = Job.new(job_params)
     if @job.save
       redirect_to @job
     else
       flash[:notice] = 'Não foi possível criar a vaga'
       @companies = Company.all
       render 'new'
-
     end
   end
 
+  def edit
+    @job = Job.find(params[:id])
+    @companies = Company.all
+  end
+
+  def update
+    @job = Job.find(params[:id])
+     if @job.update(job_params)
+      redirect_to @job
+    else
+      flash[:notice] = 'Não foi possível atualizar a vaga'
+      @companies = Company.all
+      render 'edit'
+    end
+  end
+
+  def job_params
+    params.require(:job).permit(:title, :company_id, :description, :location, :category, :featured)
+  end
 
 end
